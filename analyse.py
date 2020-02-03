01f8f53bab2aadc6b47fa61142750ee8eac3dba3
Lukas Spatschil <e11810356@student.tuwien.ac.at>
2020-02-03 18:55:21 +0100

    - still saves in cache?

--- analyse.py
+++ analyse.py
@@ -4,15 +4,18 @@ import time
 
 def main():
     file = open("data.out", 'w')
+    testCases = []
 
     for i in range(1000):
         startTime = time.time()
-        if solver.solveSudoku(boards.board.copy()):
+        if solver.solveSudoku(boards.board953.copy()):
             endTime = time.time()
         
-        file.write(str(solver.iterations) + ";" + str(round(endTime - startTime, 3)) + '\n')
+        file.write(str(solver.iterations) + ";" + str(endTime - startTime) + '\n')
+
+        file.flush()
 
     file.close()
 
-main()
-    
+main() 
+print("Done!")
