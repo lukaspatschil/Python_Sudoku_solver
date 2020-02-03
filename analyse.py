5b49c2f02435dfbd969f09af0c7847f31d11827d
Lukas Spatschil <e11810356@student.tuwien.ac.at>
2020-02-03 18:02:55 +0100

    - added basic test for multiple same boards

--- analyse.py
+++ analyse.py
@@ -1,5 +1,18 @@
 import boards
 import solver
+import time
 
 def main():
+    file = open("data.out", 'w')
+
+    for i in range(1000):
+        startTime = time.time()
+        if solver.solveSudoku(boards.board):
+            endTime = time.time()
+        
+        file.write(str(solver.iterations) + " " + str(round(endTime - startTime, 3)))
+
+    file.close()
+
+main()
     
